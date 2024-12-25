c3198405e381473a21e09a8156365d4cf96ef714
omarelsadanyy <omarelsadany600@gmail.com>
2024-12-25 22:47:09 +0200

    done

--- Bank_Account.dart
+++ Bank_Account.dart
@@ -1,13 +1,14 @@
 import 'dart:ffi';
 
 void main(List<String> args) {
+  //object from the class
   var user_One= BankAccount.ZeroBalance("omar elsadany");
   user_One.withDraw(200);
 
   user_One.deposit(500);
 
   user_One.displayInfo();
-
+  //object from the class
   var user_Two= BankAccount("omar elsadany",600);
   user_One.withDraw(900);
 
@@ -20,7 +21,7 @@ void main(List<String> args) {
 
 
 
-
+ // Bank account class
 class BankAccount {
   String account;
   double balance;
@@ -28,24 +29,28 @@ class BankAccount {
   BankAccount(this.account,this.balance);
 
   BankAccount.ZeroBalance(this.account) : balance=0;
-
+ 
   void withDraw(double amount){
+    // check if positive or not
     if(amount <=0){
        print("amount should be positive or above Zero");
+       // check if the amount more than the current balance
     }else if(amount > balance){
         print("Insuffecient Amount");
     }else{
+      // perform transaction
       balance-=amount;
       print("you withdrawed :$amount  and your Balance now is :$balance");
     }
   }
-
+ 
   void deposit(double amount){
-
+    // Add more to your balance
     balance+=amount;
     print("Your Balance now is : $balance");
   }
-
+  
+  // display info
   void displayInfo(){
     print("AccountId: $account");
     print("Your Balance is : $balance");
